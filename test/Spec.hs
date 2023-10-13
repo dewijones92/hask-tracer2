c7fcffe6a0b142dfc579ce6076892ac101fc1682
Dewi Jones <dewijones92@gmail.com>
2023-10-13 15:46:05 +0100

    hls works

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,47 +1,49 @@
-import qualified System.Exit as Exit
- 
-import Control.Exception  -- Imports everything
-import Control.Exception (ArithException(..))
-
-import Control.Monad (guard)
-import Lib (insertTraceShows, addTrace)  -- Importing functions from your main code
-import Language.Haskell.Exts
-import Data.Generics
-
-import Test.Tasty
-import Test.Tasty.HUnit
 import System.IO.Silently (capture_)
+import System.Exit (exitFailure)
+import Test.HUnit
+import Lib (someFunc, insertTraceShows, addTrace)
 import Language.Haskell.Exts
-import Lib (someFunc, insertTraceShows, addTrace)  -- Import your Lib module here
+import Data.List (tails, isPrefixOf)  -- <-- Added this import
+
+-- Helper function to check if a string contains a substring
+contains :: String -> String -> Bool
+contains str substr = any (== substr) (tails str)
+
+-- Helper function to check if a string represents an error
+isError :: String -> Bool
+isError = ("Error" `isPrefixOf`)
+
+-- Test for someFunc
+testSomeFunc :: Test
+testSomeFunc = TestCase $ do
+    output <- capture_ someFunc
+    assertEqual "Should print 'someFunc\\n'" "someFunc\n" output
+
+-- Test for insertTraceShows
+testInsertTraceShows :: Test
+testInsertTraceShows = TestList [
+    TestCase $ do
+        let input = "main = print 42"
+        let output = insertTraceShows input
+        assertBool "Should insert trace statements" (output `contains` "traceShow"),
+    TestCase $ do
+        let input = "main = print 42;"
+        let output = insertTraceShows input
+        assertBool "Should fail on syntax errors" (isError output)
+  ]
+
+-- Test for addTrace
+testAddTrace :: Test
+testAddTrace = TestCase $ do
+    let input = parseExp "x + y"
+    let output = case input of
+                    ParseOk expr -> prettyPrint (addTrace expr)
+                    ParseFailed _ _ -> "Error"
+    assertEqual "Should add trace to expression" "traceShow (x + y) (x + y)" output
 
 main :: IO ()
-main = defaultMain $ testGroup "Lib Tests"
-    [ testCase "test someFunc" testSomeFunc
-    , testCase "test insertTraceShows" testInsertTraceShows
-    , testCase "test addTrace" testAddTrace
-    ]
-
-testSomeFunc :: Assertion
-testSomeFunc = do
-    output <- capture_ someFunc
-    output @?= "someFunc\n"
-
-testInsertTraceShows :: Assertion
-testInsertTraceShows = do
-    let input = "let x = 5 in x + 3"
-    let expected = "your expected output here"  -- Specify the expected output
-    let output = insertTraceShows input
-    output @?= expected
-
-testAddTrace :: Assertion
-testAddTrace = do
-    let inputExp = parseValidExp "5 + 3"
-    let expectedExp = parseValidExp "traceShow (5 + 3) (5 + 3)"
-    let outputExp = addTrace inputExp
-    prettyPrint outputExp @?= prettyPrint expectedExp
-
--- Utility function to parse an expression and assume it's valid.
-parseValidExp :: String -> Exp SrcSpanInfo
-parseValidExp str = case parseExp str of
-    ParseOk exp -> exp
-    ParseFailed srcLoc err -> error $ show srcLoc ++ ": " ++ err
+main = do
+    counts <- runTestTT $ TestList [testSomeFunc, testInsertTraceShows, testAddTrace]
+    if errors counts + failures counts == 0
+        then return ()
+        else exitFailure  -- You need to import System.Exit for this
