25a2d7df04b33fe3bf35700f3481a7211132786c
Dewi Jones <dewijones92@gmail.com>
2023-11-19 07:23:21 +0000

    test autodiscover tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,1 @@
-import Test.Hspec
-import Test.HUnit
-import DewiSpec2 (spec) -- Importing the spec function from DewiSpec2
-
-main :: IO ()
-main = hspec $ do
-  describe "asdsad" DewiSpec2.spec
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
