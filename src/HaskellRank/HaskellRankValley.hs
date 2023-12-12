a764cb65b96c8e42a3c5c0fd657d48af181d4dd0
Dewi Jones <dewijones92@gmail.com>
2023-12-12 08:06:09 +0000

    more

--- src/HaskellRank/HaskellRankValley.hs
+++ src/HaskellRank/HaskellRankValley.hs
@@ -1,18 +1,38 @@
 module HaskellRank.HaskellRankValley
-    ( main, countingValleys
+    ( main, countingValleys, solve
     ) where
 
-countingValleys :: Int -> [Char] -> Int
+-- Define custom data types
+-- Define custom data types
+newtype Altitude = Altitude Int deriving (Eq, Show)
+data Step = Up | Down deriving (Eq, Show)
+
+-- Convert Char to Step
+charToStep :: Char -> Maybe Step
+charToStep 'U' = Just Up
+charToStep 'D' = Just Down
+charToStep _   = Nothing
+
+-- Main function with enhanced type safety
+countingValleys :: Altitude -> [Step] -> Int
 countingValleys _ [] = 0
-countingValleys altitude (step:path) = 
-    let newAltitude = if step == 'U' then altitude + 1 else altitude - 1
-        addValley = if altitude == -1 && step == 'U' then 1 else 0
+countingValleys (Altitude altitude) (step:path) =
+    let newAltitude = case step of
+                        Up   -> Altitude (altitude + 1)
+                        Down -> Altitude (altitude - 1)
+        addValley = if altitude == -1 && step == Up then 1 else 0
     in addValley + countingValleys newAltitude path
 
 
 
+solve :: String -> String
+solve input = do
+    let steps = [step | Just step <- map charToStep input]
+    let initialAltitude = Altitude 0
+    show $ countingValleys initialAltitude steps
+
 main :: IO ()
 main = do
     inputLine <- readLn
-    putStrLn $ show $ countingValleys 0 inputLine
-
+    let pathString = inputLine
+    putStrLn $ solve pathString
--- test/HaskellRank/HaskellRankValleySpec.hs
+++ test/HaskellRank/HaskellRankValleySpec.hs
@@ -3,7 +3,7 @@ import Test.Hspec ( describe, it, Spec )
 import Test.Hspec
 import Test.HUnit
 
-import HaskellRank.HaskellRankValley ( countingValleys )
+import HaskellRank.HaskellRankValley ( solve )
 
 assertEqual' :: (Eq a, Show a) => a -> a -> Assertion
 assertEqual' = assertEqual ""
@@ -12,6 +12,6 @@ spec :: Spec
 spec = do 
   describe "go" $ do
     it "odd length string" $ do
-      let valleyCount = countingValleys 0 "DUUDUUUUUD"
-      assertEqual'  10 valleyCount
+      let valleyCount = solve "DUUDUUUUUD"
+      assertEqual'  "10" valleyCount
 
