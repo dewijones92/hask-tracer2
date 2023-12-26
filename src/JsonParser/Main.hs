f2b126169eaca6afd87e64a383b718a9922c1556
Dewi Jones <dewijones92@gmail.com>
2023-12-26 11:03:43 +0000

    more

--- src/JsonParser/Main.hs
+++ src/JsonParser/Main.hs
@@ -2,11 +2,13 @@
 module JsonParser.Main
     ( main
     ) where
-import Data.Char (isDigit)
+import Data.Char
 import Data.Char (isDigit, digitToInt)
 import Text.Read.Lex (Number)
 import Control.Arrow
 import GHC.IO.Handle (NewlineMode(inputNL))
+import Control.Applicative
+import Text.Megaparsec.Byte (string)
 
 data JsonValue =  JsonNull
                                | JsonBool Bool
@@ -28,18 +30,26 @@ instance Functor Parser where
                         return (input', f x)
 
 
-
+instance Applicative Parser where
+    pure x = Parser $ \input -> Just (input, x)
+    (Parser p1) <*> (Parser p2) =Parser $  \input -> do
+                                                    (input', f ) <- p1 input
+                                                    (input'', a) <- p2 input'
+                                                    Just (input'', f a)
 
 newtype Parser a =
      Parser { runParser :: String -> Maybe (String, a) }
 
 jsonNull :: Parser JsonValue
-jsonNull = undefined
+jsonNull =  (\_ -> JsonNull) <$> stringP "null"
 
 charP :: Char -> Parser Char
 charP x = Parser $ \case
       y:ys | y == x -> Just (ys, x)
       _ -> Nothing
+
+stringP :: String -> Parser String
+stringP = sequenceA . map charP
 
 -- Parses a single digit and returns the rest of the string
 digitParser :: Parser Int
@@ -48,11 +58,10 @@ digitParser = Parser $ \input -> case input of
     _ -> Nothing
 
 
-dewi :: Int -> Int
-dewi p =
-    let valuex = 1
-        valuey = 2
-    in valuex + valuey
+-- Sample parser that parses an integer
+parseInt :: Parser Int
+parseInt = Parser $ \s -> Just (tail s, read [head s])
+
 
 
 main :: IO ()
