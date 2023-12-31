5fc49df377c9e65aa28d0d80d640b0847b6d959d
Dewi Jones <dewijones92@gmail.com>
2023-12-31 07:49:15 +0000

    WORKS

--- src/HaskellMusic/HaskellMusic.hs
+++ src/HaskellMusic/HaskellMusic.hs
@@ -6,19 +6,39 @@ import qualified Data.ByteString.Lazy as B.L
 import qualified Data.ByteString.Builder as B.B
 import qualified Data.Foldable as Data
 import Data.Foldable
+import Test.QuickCheck (sample)
+
+type Seconds = Float
+type Samples = Float
+type Hz = Float
+type Pulse = Float
 
 volume :: Float
 volume = 0.5
 
-sampleRate :: Float
+sampleRate :: Samples
 sampleRate = 48000
 
 frequency :: Float
 frequency = 440 -- Middle A
 
-wave :: [Float]
-wave = map ((*volume).sin.(*step)) [0.0 .. sampleRate]
-  where step = 0.05
+
+
+freq :: Hz -> Seconds -> [Pulse]
+frez hz duration = map ((*volume).sin.(*step)) [0.0 .. sampleRate * duration]
+  where
+    step = (hz * 2 * pi) / sampleRate
+
+wave :: [Pulse]
+wave = freq hz duration
+  where 
+    duration :: Seconds
+    duration = 2.0
+    step = (hz * 2 * pi) / sampleRate
+    hz :: Float
+    hz = 440.0
+
+
 
 save :: IO ()
 save = B.L.writeFile "output.bin" $ B.B.toLazyByteString $ fold $ map B.B.floatBE wave
