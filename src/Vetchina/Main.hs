63c3c08c73b99259d01e9ff247fd4a5ac5511c4e
Dewi Jones <dewijones92@gmail.com>
2024-01-02 06:31:12 +0000

    more

--- src/Vetchina/Main.hs
+++ src/Vetchina/Main.hs
@@ -3,18 +3,27 @@ module Vetchina.Main
     ) where
 
 import qualified Data.Map as M
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as E
+
 import Data.Foldable
 import Data.Maybe
+import qualified Data.ByteString as T
+import Graphics.Gloss (text)
+import System.Directory
  
-newtype Bow = Bow  { bowToMap :: M.Map String Int } deriving (Show )
+newtype Bow = Bow  { bowToMap :: M.Map T.Text Int } deriving (Show, Read )
 
-wordToBow :: String -> Bow
+wordToBow :: T.Text -> Bow
 wordToBow w = Bow $ M.fromList [(w,1)]
+
+textToBow :: T.Text -> Bow
+textToBow = foldMap wordToBow . T.words
  
 wordsCount :: Bow -> Int
 wordsCount (Bow bow) = sum $ map snd $ M.toList bow
 
-wordProbability :: String -> Bow -> Float
+wordProbability :: T.Text -> Bow -> Float
 wordProbability w bow = fromIntegral n / fromIntegral (wordsCount bow)
     where n = fromMaybe 0 $ M.lookup w $ bowToMap bow
 
@@ -26,5 +35,24 @@ instance Semigroup Bow where
 instance Monoid Bow where 
     mempty = emptyBow
 
+bowFromFile :: FilePath -> IO Bow
+bowFromFile  filePath = textToBow. E.decodeUtf8
+                                             <$> T.readFile filePath
+
+bowFromFolder :: FilePath -> IO Bow
+bowFromFolder folderPath = do
+    fileNames <- listDirectory folderPath
+    bows <- mapM (\fileName -> bowFromFile (folderPath <> fileName)) fileNames
+    return $ fold bows
+
+baseDir = "/home/dewi/Downloads/Email-Classification-Spam-or-Ham/E-mail_Classification/"
+
+spamBow :: IO Bow
+spamBow = bowFromFolder  (baseDir ++ "spam/")
+
+hamBow :: IO Bow
+hamBow = bowFromFolder  (baseDir ++ "ham/")
+
 main :: IO ()
-main  = undefined 
+main  = do
+    undefined
