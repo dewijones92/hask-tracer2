4069d467a904101a0c11980a13e33637f6f322c2
Dewi Jones <dewijones92@gmail.com>
2024-01-04 08:31:31 +0000

    works

--- src/Vetchina/Main.hs
+++ src/Vetchina/Main.hs
@@ -6,6 +6,7 @@ import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as E
 import Data.Char
+import Data.Maybe
 
 import Data.Foldable
 import Data.Maybe
@@ -15,6 +16,7 @@ import System.Directory
 import Data.List (sortBy)
 import Text.Printf
 import Data.Function
+import qualified GHC.Base as IO
 
 newtype Word'= Word' T.Text deriving (Show, Read, Eq, Ord)
 
@@ -28,7 +30,7 @@ normalizeTextToWords :: T.Text -> [Word']
 normalizeTextToWords =
     map mkWord .
     T.words .
-    T.map (\x -> 
+    T.map (\x ->
                     if isAlphaNum x
                         then x
                         else ' ')
@@ -40,7 +42,7 @@ wordToBow w = Bow $ M.fromList [(w,1)]
 
 textToBow :: T.Text -> Bow
 textToBow = foldMap wordToBow . normalizeTextToWords
- 
+
 wordsCount :: Bow -> Int
 wordsCount (Bow bow) = sum $ map snd $ M.toList bow
 
@@ -53,12 +55,20 @@ wordProbability :: Word' -> Bow -> Float
 wordProbability w bow = fromIntegral n / fromIntegral (wordsCount bow)
     where n = fromMaybe 0 $ M.lookup w $ bowToMap bow
 
+seenWord :: SpamFilter -> Word' -> Bool
+seenWord (SpamFilter (Bow spamBow) (Bow hamBow)) w =  isJust sm || isJust hm
+ where
+     sm = M.lookup w  spamBow
+     hm = M.lookup w  hamBow
+
+
+
 emptyBow = Bow M.empty
 
 instance Semigroup Bow where
     Bow bow1 <> Bow bow2 = Bow $ M.unionWith (+) bow1 bow2
 
-instance Monoid Bow where 
+instance Monoid Bow where
     mempty = emptyBow
 
 bowFromFile :: FilePath -> IO Bow
@@ -73,19 +83,34 @@ bowFromFolder folderPath = do
 
 baseDir = "/home/dewi/Downloads/Email-Classification-Spam-or-Ham/E-mail_Classification/"
 
-spamBow :: IO Bow
-spamBow = bowFromFolder  (baseDir ++ "train/spam/")
+data SpamFilter = SpamFilter { spamBow :: Bow,
+                                                                    hamBow :: Bow}
 
-hamBow :: IO Bow
-hamBow = bowFromFolder  (baseDir ++ "train/ham/")
+spamFilter :: IO SpamFilter
+spamFilter =  do
+    spam <- bowFromFolder (baseDir ++ "train/spam/")
+    ham <-   bowFromFolder (baseDir ++ "train/ham/")
+    return $ SpamFilter spam ham
 
-wordProbabilitySpam :: Word' -> IO Float
-wordProbabilitySpam w = do
-    pws <- wordProbability w <$> spamBow
-    phs <- wordProbability w <$> hamBow
-    let ps = pws + phs
-    return $  if ps == 0.0 then 0.0 else pws / (pws + phs)
 
+
+wordProbabilitySpam :: SpamFilter -> Word' -> Maybe Float
+wordProbabilitySpam sf@(SpamFilter spamBow hamBow) w
+    | seenWord sf w =
+        let
+            pws = wordProbability w spamBow
+            phs = wordProbability w hamBow
+            ps = pws + phs
+        in Just (pws / (pws/phs))
+    | otherwise = Nothing
+
+textProbabilitySpam :: T.Text -> SpamFilter -> Float
+textProbabilitySpam text sm = pp / (pp + product ips)
+        where
+            ws = normalizeTextToWords text
+            ps = mapMaybe (wordProbabilitySpam sm) ws
+            ips = map (1.0 -) ps
+            pp = product ps
 
 main :: IO ()
 main  = do
